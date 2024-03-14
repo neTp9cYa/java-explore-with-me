ec0c1a7982d13384ca4275a97d20bcd76e257912
Alexander Petryakov <neTp9c@yandex.ru>
2024-03-14 22:59:15 +0300

    fix response code when create event

--- ewm-service/src/main/java/ru/practicum/ewm/service/controller/privateArea/EventPrivateController.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/controller/privateArea/EventPrivateController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.ewm.service.dto.event.EventCreateRequestDto;
 import ru.practicum.ewm.service.dto.event.EventFullDto;
@@ -34,6 +36,7 @@ public class EventPrivateController {
     private final EventService eventService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     @LogInputOutputAnnotaion
     public EventFullDto create(@PathVariable final long userId,
                                @RequestBody @Valid final EventCreateRequestDto eventCreateRequestDto) {
